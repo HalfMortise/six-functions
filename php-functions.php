729cace86f065fb62c85d9efbbc078f96b51d859
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-16 14:50:15 -0600

    Completed string function

--- php-functions.php
+++ php-functions.php
@@ -10,11 +10,34 @@
 /*Array Function: sorting first row of students by age in descending order
 
 //Define array
-$age = array("Joy"=>35, "Jude"=>32, "Chelsea"=>29, "Sarah"=>24.5, "Ryo"=> 24);
+$age = array("Joy"=>35, "Jude"=>32, "Joe"=>31, "Sarah"=>24.5, "Ryo"=> 24);
 
 //Printing array
 arsort($age);
 print_r($age);*/
+//
+//
+//
+/*
+//Simple password generator (don't actually use this for real passwords!)
+//The password length will be 15 characters
+function generatePasswordLength($length = 15) {
+	$possibleChars = "abcdefghijklmnopqrstuvwxyz123456789)(*&^%$#@!";
+	$password = '';
+//The character output will loop up to 15 characters
+	for($i = 0; $i < $length; $i++) {
+		//This is randomizing the output of the characters
+		$rand = rand(0, strlen($possibleChars) - 1);
+		$password .= substr($possibleChars, $rand, 1);
+	}
+//This is where the function is called
+	return $password;
+}
+*/
+//
+//
+//
+		//		//
 ?>
 	</body>
 </html?
