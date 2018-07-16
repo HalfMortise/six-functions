05559e4c6e44a46d0cdd261ffe546d45fa07398a
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-16 14:16:15 -0600

    Completed array function

---
+++ js-functions.js
@@ -0,0 +1,24 @@
+/*
+//Array function: sorting first row of students by age in descending order
+
+let ages = [{
+	"age": "24",
+	"name": "Ryo",
+}, {
+	"age": "24.5",
+	"name": "Sarah",
+}, {
+	"age": "31",
+	"name": "Joe",
+}, {
+	"age": "35",
+	"name": "Joy",
+}, {
+	"age": "32",
+	"name": "Jude",
+}];
+
+ages.sort((a,b) => Number(a.age) - Number(b.age));
+console.log("descending", ages);
+*/
+
