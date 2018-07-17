b011e8d8cb67d052432a29341cb44ac628ff9d52
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-16 22:20:11 -0600

    Updated note

--- js-array-functions.js
+++ js-array-functions.js
@@ -1,6 +1,6 @@
 //This doc contains only the Array Function
 //ARRAY FUNCTION: sorting first row of students by age in ascending order
-
+//Run in Node.js to see the array
 let ages = [{
 	"age": "24",
 	"name": "Ryo",
