cdc379be4cead293567f15c14041237d74eb07ae
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-16 22:06:11 -0600

    Final updates and notes applied

---
+++ js-array-functions.js
@@ -0,0 +1,22 @@
+//This doc contains only the Array Function
+//ARRAY FUNCTION: sorting first row of students by age in ascending order
+
+let ages = [{
+	"age": "24",
+	"name": "Ryo",
+}, {
+	"age": "24.5",
+	"name": "Sarah",
+}, {
+	"age": "31",
+	"name": "Joe",
+}, {
+	"age": "35",
+	"name": "Joy",
+}, {
+	"age": "32",
+	"name": "Jude",
+}];
+
+ages.sort((a,b) => Number(a.age) - Number(b.age));
+console.log("descending", ages);
